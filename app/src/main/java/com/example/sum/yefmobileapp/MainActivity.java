4c24074752420d9be6b387275a648307ab557a47
some1-unknown <sharshil1299@gmail.com>
2018-06-23 12:18:30 +0530

    Basic Layout Ready

--- app/src/main/java/com/example/sum/yefmobileapp/MainActivity.java
+++ app/src/main/java/com/example/sum/yefmobileapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.sum.yefmobileapp;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("activity","In Main Activity");
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch( item.getItemId()){
+            case R.id.menuhome:
+                Toast.makeText(this,"Home Clicked",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menuchatwithus:
+                Toast.makeText(this,"Chat Clicked",Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(MainActivity.this, chat.class));
+                return true;
+            case R.id.menuaboutus:
+                Toast.makeText(this,"About Us Clicked",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, aboutus.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
