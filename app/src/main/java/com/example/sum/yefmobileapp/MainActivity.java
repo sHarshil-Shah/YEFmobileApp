a23ed3760b20ddfbf22693833941af2b899ee875
some1-unknown <sharshil1299@gmail.com>
2018-06-23 16:23:12 +0530

    Main Page Updated with Information

--- app/src/main/java/com/example/sum/yefmobileapp/MainActivity.java
+++ app/src/main/java/com/example/sum/yefmobileapp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.sum.yefmobileapp;
 
-import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,37 +22,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("activity","In Main Activity");
+        Log.d("activity", "In Main Activity");
+
+        final ViewPager viewPager=findViewById(R.id.viewPager);
+        Comunicator comunicator=new Comunicator(getSupportFragmentManager());
+        viewPager.setAdapter(comunicator);
+        viewPager.setCurrentItem(0);
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.ic_yef_logo_round);
-        }
+
+        findViewById(R.id.txtGeneral).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0);
+            }
+        });
+        findViewById(R.id.txtSkills).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mymenu,menu);
+        getMenuInflater().inflate(R.menu.mymenu, menu);
         return true;
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch( item.getItemId()){
-            case R.id.menuhome:
-                Toast.makeText(this,"Home Clicked",Toast.LENGTH_SHORT).show();
-                return true;
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            switch (item.getItemId()) {
 
-            case R.id.menuchatwithus:
-                Toast.makeText(this,"Chat Clicked",Toast.LENGTH_SHORT).show();
-
-                startActivity(new Intent(MainActivity.this, chat.class));
-                return true;
-            case R.id.menuaboutus:
-                Toast.makeText(this,"About Us Clicked",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, aboutus.class));
-                return true;
+                case R.id.menuchatwithus:
+                    startActivity(new Intent(MainActivity.this, chat.class));
+                    return true;
+                case R.id.menuaboutus:
+                    startActivity(new Intent(MainActivity.this, aboutus.class));
+                    return true;
+            }
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+
     }
-}
