12aec4cbca5952524223acecedb3e428f554f67b
some1-unknown <sharshil1299@gmail.com>
2018-06-23 23:35:40 +0530

    Removed Fragments and Registration Page

--- app/src/main/java/com/example/sum/yefmobileapp/MainActivity.java
+++ app/src/main/java/com/example/sum/yefmobileapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sum.yefmobileapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -18,28 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("activity", "In Main Activity");
 
-        final ViewPager viewPager=findViewById(R.id.viewPager);
-        Comunicator comunicator=new Comunicator(getSupportFragmentManager());
-        viewPager.setAdapter(comunicator);
-        viewPager.setCurrentItem(0);
+
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.ic_yef_logo_round);
 
-        findViewById(R.id.txtGeneral).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewPager.setCurrentItem(0);
-            }
-        });
-        findViewById(R.id.txtSkills).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewPager.setCurrentItem(1);
-            }
-        });
     }
 
     @Override
@@ -58,9 +44,6 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.menuaboutus:
                     startActivity(new Intent(MainActivity.this, aboutus.class));
-                    return true;
-                case R.id.register:
-                    startActivity(new Intent(MainActivity.this, registration.class));
                     return true;
             }
             return super.onOptionsItemSelected(item);
